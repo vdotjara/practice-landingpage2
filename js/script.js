6d562444c676c2cdb31f0a91c03a275b6ec95ab6
victor jara <vjarafer@gmail.>
2024-06-03 18:25:16 -0400

    added mobile menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+const logo = document.getElementById('logo')
 
 /* Tabs menu event listener */
-
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+
+/* Hambuerger Button Listener */
+btn.addEventListener('click', navToggle)
 
 function onTabClick(e) {
   /* Deactivate all tabs */
@@ -25,4 +30,16 @@ function onTabClick(e) {
     .getElementById("panels")
     .getElementsByClassName(classString)[0]
     .classList.remove('hidden');
+}
+
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+
+  if(menu.classList.contains('flex')){
+    logo.setAttribute('src', './img/logo-bookmark-footer.svg')
+  } else {
+    logo.setAttribute('src', './img/logo-bookmark.svg')
+  }
 }
